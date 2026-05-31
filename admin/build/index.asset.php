c2a9beba08be581a58bad2826ef1b24bd8da976e
Nazmul Hasan Robin <robin.sust08@gmail.com>
2026-05-31 22:48:14 +0600

    Update asset version and modify POT creation date
    - Updated the version in index.asset.php to reflect the latest build.
    - Adjusted the POT-Creation-Date in nhrrob-smart-media-manager.pot for accurate translation file generation.
    - Enhanced index.js with improved folder handling and user feedback mechanisms.

--- admin/build/index.asset.php
+++ admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'cf93d56242d3eaa04672');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'c695ea93722d1781c1f6');
